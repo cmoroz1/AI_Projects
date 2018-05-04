912c24c7a7635e53a6d8f5703ebdb33d8002883d
cmoroz1 <cmoroz1@tulane.edu>
2018-05-04 10:10:43 -0500

    Finished Q3 in Tutorial

--- Tutorial/shopSmart.py
+++ Tutorial/shopSmart.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -28,8 +28,14 @@ def shopSmart(orderList, fruitShops):
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
-    "*** YOUR CODE HERE ***"
-    return None
+    min_cost = fruitShops[0].getPriceOfOrder(orderList)
+    min_shop = fruitShops[0]
+    for fruitShop in fruitShops:
+        cost = fruitShop.getPriceOfOrder(orderList)
+        if cost < min_cost:
+            min_cost = cost
+            min_shop = fruitShop
+    return min_shop
 
 if __name__ == '__main__':
   "This code runs when you invoke the script from the command line"
